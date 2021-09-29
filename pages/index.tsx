9bb60b8e5a9d2a0904fd26f0aebbb8cf97c428e0
Gage Bachik <loser@gagebachik.com>
2021-09-28 21:08:10 -0700

    vercel releases working and daisy ui base setup for project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,11 @@
 import type { NextPage } from "next";
-import AudioViz from "../components/AudioViz";
+import NavBar from "../components/NavBar";
 
 const Home: NextPage = () => {
   return (
-    <main>
-      <div className="h-[100vh] flex flex-col justify-center align-middle text-center">
-        <h1 className="text-[72px] bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 font-extrabold">
-          Batteries Included Next.js
-        </h1>
-        <h2 className="text-2xl max-w-md mx-auto">
-          A Next.js Boilerplate with TypeScript, Tailwind CSS and testing suite
-          enabled
-        </h2>
-        {/* <VanillaAudioViz /> */}
-        <AudioViz />
+    <main className="p-4 lg:p-18">
+      <div className="grid grid-cols-1 gap-6 lg:p-10 xl:grid-cols-3 lg:bg-base-200 rounded-box">
+        <NavBar />
       </div>
     </main>
   );
